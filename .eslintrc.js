929921dad5c7c3dabef6d43bfcd12bb799a7d7f4
yu-heejin <jkgt0521@naver.com>
2023-05-03 21:19:40 +0900

    feat: prettier 설정하기 [#1]

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,10 +27,10 @@ module.exports = {
     rules: {
         indent: ['error', 2, { SwitchCase: 1 }],
         quotes: ['error', 'single', { avoidEscape: true }],
-
         'no-empty-function': 'off',
         '@typescript-eslint/no-empty-function': 'off',
         'react/display-name': 'off',
         'react/prop-types': 'off',
+        'prettier/prettier': 'error',
     }
 }
