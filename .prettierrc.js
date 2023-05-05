2de9d4abc76d13767d3280d0319efa6139c5c2d2
yu-heejin <jkgt0521@naver.com>
2023-05-05 17:11:02 +0900

    refactor: prettier 설정 변경 [#1]

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,12 @@
 module.exports = {
-    arrowParens: 'avoid',
-    singleQuote: true,
-    tabWidth: 4,
-    trailingComma: 'all',
-    printWidth: 120,
-    endOfLine: 'auto',
+    "semi": true,
+    "tabWidth": 2,
+    "trailingComma": "es5",
+    "printWidth": 120,
+    "singleQuote": true,
+    "useTabs": false,
+    "bracketSpacing": true,
+    "jsxBracketSameLine": false,
+    "arrowParens": "always",
+    "jsxSingleQuote": true,
 };
