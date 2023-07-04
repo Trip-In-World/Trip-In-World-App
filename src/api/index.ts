4a3fa901d8803c71063f087b5b12b0eb72e43533
yu-heejin <jkgt0521@naver.com>
2023-07-05 03:42:55 +0900

    feat: axios 인터셉터 적용 [#8]

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,25 @@
 import axios from 'axios';
 import { Platform } from 'react-native';
+import EncryptedStorage from 'react-native-encrypted-storage';
 
-const request = axios.create({
+const requestTo = axios.create({
     baseURL: Platform.OS === 'ios'
         ? process.env.IOS_REQUEST_URL
         : process.env.ANDROID_REQUEST_URL,
     timeout: 30000,
     withCredentials: true,
 });
+
+requestTo.interceptors.request.use(
+    async (config) => {
+        // TODO: redux 사용 변경
+        const accessToken = await EncryptedStorage.getItem('accessToken');
+        config.headers['Authorization'] = `Bearer ${accessToken}`;
+    }
+)
+
+requestTo.interceptors.response.use(
+    (response) => {
+        
+    }
+)
