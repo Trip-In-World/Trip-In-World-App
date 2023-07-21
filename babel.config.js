83f1ae80e7c06f770f7a1a73cef116145edc4361
yu-heejin <jkgt0521@naver.com>
2023-07-21 23:51:45 +0900

    fix: module alias 삭제 [#8]

--- babel.config.js
+++ babel.config.js
@@ -1,25 +1,3 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['./src'],
-        extensions: [
-          '.ios.ts',
-          '.android.ts',
-          '.ts',
-          '.ios.tsx',
-          '.android.tsx',
-          '.tsx',
-          '.jsx',
-          '.js',
-          '.json',
-        ],
-        alias: {
-          '@': './src',
-        },
-      },
-    ],
-  ],
 };
