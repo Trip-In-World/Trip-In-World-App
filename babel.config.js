f82d4e9d4642072dde5a2b9442a49c01df75f4f2
yu-heejin <jkgt0521@naver.com>
2023-07-22 01:53:06 +0900

    feat: module alias 설정 [#8]

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,31 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset']
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        root: ['.'],
+        alias: {
+          '@assets': './src/assets/',
+          '@components': './src/components/',
+          '@config': './src/config/',
+          '@hooks': './src/hooks/',
+          '@navigation': './src/navigation/',
+          '@screens': './src/screen/',
+          '@stores': './src/stores/',
+        }
+      }
+    ]
+  ]
 };
