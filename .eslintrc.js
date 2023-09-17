86669dccfab4db6aa3f907f110944ff68d05a4fc
yu-heejin <jkgt0521@naver.com>
2023-09-17 12:33:47 +0900

    :sparkles:  feat: eslint 재설정 [#13]

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react/recommended'
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'@typescript-eslint',
+		'react'
+	],
+	'rules': {
+		'indent': [
+			'warn',
+			'tab'
+		],
+		'quotes': [
+			'warn',
+			'single'
+		],
+		'semi': [
+			'warn',
+			'always'
+		]
+	}
+};
